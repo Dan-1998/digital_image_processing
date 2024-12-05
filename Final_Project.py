b97068205ad77399fb3839bb3bea4c2f29119486
Daniel Rozhanskiy <rozhanskiyd@hotmail.com>
2024-12-05 10:42:30 -0600

    updates

--- Final_Project.py
+++ Final_Project.py
@@ -18,20 +18,6 @@ def unnormalize(pixel):
 
 def convert(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-# def gray_world(img):
-#     dim = np.shape(img)[2]
-#     img = np.array(img, dtype='uint8')
-#     out = np.zeros(np.shape(img))
-#     avg = np.mean(np.mean(img))
-#     # 想较原始gray_world有改动
-#     for j in range(0, dim):
-#         m = np.sum(np.sum(img[:, :, j], axis=0), axis=0)
-#         n = np.size(img[:, :, j])
-#         scale = n/m
-#         g_weight = (avg*scale)
-#         out[:, :, j] = img[:, :, j]*g_weight
-#     out = np.array(out, dtype='uint8')
-#     return out
     
 def gray_world(img):
     # Convert image to float32 for precision during calculations
@@ -62,28 +48,6 @@ def gray_world(img):
     img_corrected = np.uint8(img_float)
 
     return img_corrected
-
-# def white_balanced_image(img):
-#     img = np.double(img)
-#     R = img[:, :, 2]
-#     G = img[:, :, 1]
-#     B = img[:, :, 0]
-#     # 三颜色通道均值，对应 I¯r I¯g I¯b
-#     Irm = np.mean(R, axis=0)
-#     Irm = np.mean(Irm)/256.0
-#     Igm = np.mean(G, axis=0)
-#     Igm = np.mean(Igm)/256.0
-#     Ibm = np.mean(B, axis=0)
-#     Ibm = np.mean(Ibm)/256.0
-#     a = 1
-#     Irc = R + a * (Igm-Irm)*(1-Irm)*G  # 补偿红色通道
-#     Irc = np.array(Irc.reshape(G.shape), np.uint8)
-#     Ibc = B + a * (Igm-Ibm)*(1-Ibm)*G  # 补偿蓝色通道
-#     Ibc = np.array(Ibc.reshape(G.shape), np.uint8)
-
-#     G = np.array(G, np.uint8)
-#     img = cv2.merge([Ibc, G, Irc])
-#     return img
     
 def white_balanced_image(img, alpha=1, compensate_blue_channel=False):
     """
@@ -96,7 +60,6 @@ def white_balanced_image(img, alpha=1, compensate_blue_channel=False):
     """
     if len(img.shape) < 3:
         raise ValueError("Color image expected, received grayscale image")
-    #img = img.copy()
     mean_red = np.mean(img[:, :, 2])
     mean_green = np.mean(img[:, :, 1])
     mean_blue = np.mean(img[:, :, 0]) if compensate_blue_channel else None
@@ -105,30 +68,7 @@ def white_balanced_image(img, alpha=1, compensate_blue_channel=False):
             img[i][j][2] = unnormalize(normalize(img[i][j][2]) + alpha * (normalize(mean_green) - normalize(mean_red)) * (1 - normalize(img[i][j][2])) * normalize(img[i][j][1]))
             if compensate_blue_channel:
                 img[i][j][0] = unnormalize(normalize(img[i][j][0]) + alpha * (normalize(mean_green) - normalize(mean_blue)) * (1 - normalize(img[i][j][0])) * normalize(img[i][j][1]))
-    #cv2.imwrite("image_without_grayworld.jpg", img)
-    #img = gray_world(img)#wb.balanceWhite(img)
     return img
-
-# def normalized_unsharp_masking(image):
-#     # Convert the image to floating point format
-#     image = image.astype(np.float32)
-
-#     # Apply Gaussian smoothing
-#     im_blur = cv2.GaussianBlur(image,(7,7),0)
-
-#     # Calculate unsharp mask
-#     unsharp_mask = image - im_blur
-
-#     # Perform histogram stretching
-#     stretch_im = cv2.normalize(unsharp_mask, None, 0, 255, cv2.NORM_MINMAX) # type: ignore
-
-#     # Combine original and stretched images
-#     result = (image + stretch_im)/2
-
-#     # Convert result back to uint8 format
-#     result = np.clip(result, 0, 255).astype(np.uint8)
-
-#     return result
         
 def normalized_unsharp_masking(img, kernel_size=7):
     b, g, r = cv2.split(img)
@@ -146,12 +86,6 @@ def gamma_correction(img, gamma_factor=1.2):
 
 def laplacian_contrast_weight(img):
     return cv2.convertScaleAbs(cv2.Laplacian(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), cv2.CV_64F))
-
-# def laplacian_contrast_weight(img):
-#     img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-#     w = cv2.Laplacian(img, cv2.CV_64F)
-#     w = cv2.convertScaleAbs(w)
-#     return w
     
 def saliency_weight(img):
     lab_image = cv2.cvtColor(img, cv2.COLOR_BGR2Lab)
@@ -165,18 +99,6 @@ def saliency_weight(img):
     # Compute the L2 norm (Euclidean distance) between I_mu and I_ωhc
     saliency_weight = np.linalg.norm(mean_pixel - blurred_image, axis=2)  # Across the Lab channels
     return saliency_weight
-
-# def saliency_weight(img):
-#     gfrgb = cv2.GaussianBlur(img,(3,3),0)
-#     lab = cv2.cvtColor(gfrgb,cv2.COLOR_RGB2LAB)
-#     l = np.double(lab[:, :, 0])
-#     a = np.double(lab[:, :, 1])
-#     b = np.double(lab[:, :, 2])
-#     lm = np.mean(np.mean(l))
-#     am = np.mean(np.mean(a))
-#     bm = np.mean(np.mean(b))
-#     w = np.square(l-lm) + np.square(a-am) + np.square((b-bm))
-#     return w
 
 def saturation_weight(grayscale_img, color_img):
     grayscale_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -203,7 +125,6 @@ def gaussian_pyramid(image, levels):
     for i in range(levels - 1):
         image = cv2.pyrDown(image)
         pyramid.append(image)
-    print("gaussian", image.shape)
     return pyramid
 
 def laplacian_pyramid(image, levels):
@@ -217,7 +138,6 @@ def laplacian_pyramid(image, levels):
         laplacian_pyramid.insert(0, laplacian)
     return laplacian_pyramid
 
-# 金字塔重建
 def reconstruct_from_laplacian(pyramid):
     level = len(pyramid)
 
@@ -227,46 +147,22 @@ def reconstruct_from_laplacian(pyramid):
     output = pyramid[0]
     output=np.clip(output, 0, 255).astype(np.uint8)
     return output
-        
 
-# def gaussian_pyramid(image, levels):
-#     """
-#     Generate a Gaussian pyramid from normalised_weight
-#     """
-#     pyramid = [image]
-#     for _ in range(levels):
-#         image = cv2.pyrDown(image)
-#         pyramid.append(image)
-#     return pyramid
+def fusion(level, img_gamma_corrected, img_sharpened, combined_weight_gamma_corrected, combined_weight_sharpened):
+    input1_laplacian_pyramid=laplacian_pyramid(img_gamma_corrected,level)
+    input2_laplacian_pyramid=laplacian_pyramid(img_sharpened,level)
 
-# def laplacian_pyramid(image, levels):
-#     """
-#     Generate a Laplacian pyramid for the given image.
-#     """
-#     gaussian_pyr = gaussian_pyramid(image, levels)
-#     laplacian_pyr = []
+    combined_weight_gamma_corrected = gaussian_pyramid(combined_weight_gamma_corrected, level)
+    combined_weight_sharpened = gaussian_pyramid(combined_weight_sharpened, level)
 
-#     for i in range(levels):
-#         next_gaussian = cv2.pyrUp(gaussian_pyr[i + 1], dstsize=(gaussian_pyr[i].shape[1], gaussian_pyr[i].shape[0]))
-#         laplacian = cv2.subtract(gaussian_pyr[i], next_gaussian)
-#         laplacian_pyr.append(laplacian)
-
-#     laplacian_pyr.append(gaussian_pyr[-1])  # Add the smallest Gaussian at the top of the pyramid
-#     return laplacian_pyr
-
-# def reconstruct_from_laplacian(laplacian_pyr):
-#     """
-#     Reconstruct the image from its Laplacian pyramid.
-#     """
-#     reconstructed_image = laplacian_pyr[-1]
-
-#     for i in range(len(laplacian_pyr) - 2, -1, -1):
-#         reconstructed_image = cv2.pyrUp(reconstructed_image, dstsize=(laplacian_pyr[i].shape[1], laplacian_pyr[i].shape[0]))
-#         reconstructed_image = cv2.add(reconstructed_image, laplacian_pyr[i])
-
-#     return reconstructed_image
+    result = []
+    for i in range(level):
+        fuse=np.multiply(combined_weight_gamma_corrected[i],input1_laplacian_pyramid[i]) + np.multiply(combined_weight_sharpened[i],input2_laplacian_pyramid[i])
+        result.append(fuse)
+    result = reconstruct_from_laplacian(result).astype(np.uint8)
+    return result
     
-def underwater_image_enhancement(img, title):
+def underwater_image_enhancement(img, title, compensate_blue_channel, gamma_factor, kernel_size, laplacian_levels):
     img_normalized = normalize(img.copy())
     img_balanced = white_balanced_image(img.copy(), compensate_blue_channel=True)
     write_image(f"{title}_white_balanced.jpg", img_balanced)
@@ -277,7 +173,7 @@ def underwater_image_enhancement(img, title):
     img_sharpened = normalized_unsharp_masking(img_grayworld.copy())
     write_image(f"{title}_sharpened.jpg", img_sharpened)
     
-    img_gamma_corrected = gamma_correction(img_grayworld, gamma_factor=2)
+    img_gamma_corrected = gamma_correction(img_grayworld, gamma_factor=gamma_factor)
     write_image(f"{title}_gamma_corrected.jpg", img_gamma_corrected)
     
     grayscale_gamma_corrected = cv2.cvtColor(img_gamma_corrected, cv2.COLOR_BGR2GRAY).astype(np.float64)/255
@@ -309,90 +205,19 @@ def underwater_image_enhancement(img, title):
     write_image(f"{title}_combined_weight_gamma_corrected.jpg", unnormalize(combined_weight_gamma_corrected)) 
     write_image(f"{title}_combined_weight_sharpened.jpg", unnormalize(combined_weight_sharpened))
 
-    # # Initialize a list or dictionary to store the pyramids for each normalized weight
-    # pyramids = {}
-    
-    # # Iterate over the normalized weights
-    # for i, weight in enumerate([combined_weight_gamma_corrected, combined_weight_sharpened]):
-    #     # Create a Gaussian pyramid for each normalized weight
-    #     pyramids[f"weight_{i}"] = gaussian_pyramid(weight, levels=5)
-    level = 3
-    input1_laplacian_pyramid=laplacian_pyramid(img_gamma_corrected,level)
-    input2_laplacian_pyramid=laplacian_pyramid(img_sharpened,level)
+    result = fusion(laplacian_levels, img_gamma_corrected, img_sharpened, combined_weight_gamma_corrected, combined_weight_sharpened)
 
-    combined_weight_gamma_corrected = gaussian_pyramid(combined_weight_gamma_corrected, level)
-    combined_weight_sharpened = gaussian_pyramid(combined_weight_sharpened, level)
-
-    result = []
-    for i in range(level):
-        fuse=np.multiply(combined_weight_gamma_corrected[i],input1_laplacian_pyramid[i]) + np.multiply(combined_weight_sharpened[i],input2_laplacian_pyramid[i])
-        result.append(fuse)
-    result = reconstruct_from_laplacian(result).astype(np.uint8)
-    
-
-    # #weights for gaussian pyramids
-    # weight1 = pyramids["weight_0"]
-    # weight2 = pyramids["weight_1"]
-
-    # #Cal Laplacian pyramid
-    # levels = 5
-    # blue_gamma, green_gamma, red_gamma = cv2.split(img_gamma_corrected)
-    # blue_sharpened, green_sharpened, red_sharpened = cv2.split(img_sharpened)
-
-    # blue_gamma_laplacian  = laplacian_pyramid(blue_gamma, levels)
-    # green_gamma_laplacian  = laplacian_pyramid(green_gamma, levels)
-    # red_gamma_laplacian  = laplacian_pyramid(red_gamma, levels)
-     
-    # blue_sharpened_laplacian  = laplacian_pyramid(blue_sharpened, levels)
-    # green_sharpened_laplacian  = laplacian_pyramid(green_sharpened, levels)
-    # red_sharpened_laplacian  = laplacian_pyramid(red_sharpened, levels)
-
-    # # Ensure that the lengths of the pyramids match
-    # num_levels = len(weight1)  # This should be the same for both weight1 and weight2
-    
-    # # Initialize the lists to store the result of the weighted pyramids
-    # R_r, G_r, B_r = [], [], []
-    
-    # # Iterate over the pyramid levels
-    # for i in range(num_levels):
-    #     R_r.append(np.array(weight1[i]) * red_gamma_laplacian[i] + np.array(weight2[i]) * red_sharpened_laplacian[i])
-    #     G_r.append(np.array(weight1[i]) * green_gamma_laplacian[i] + np.array(weight2[i]) * green_sharpened_laplacian[i])
-    #     B_r.append(np.array(weight1[i]) * blue_gamma_laplacian[i] + np.array(weight2[i]) * blue_sharpened_laplacian[i])
-    
-    # # Rebuild the pyramids from the weighted levels
-    # R = reconstruct_from_laplacian(R_r)
-    # G = reconstruct_from_laplacian(G_r)
-    # B = reconstruct_from_laplacian(B_r)
-
-    
-    # # Combine the channels back to a color image
-    # result = cv2.merge([B, G, R])
-
-    # R[R < 0] = 0
-    # R[R > 255] = 255
-    # R = R.astype(np.uint8)
-     
-    # G[G < 0] = 0
-    # G[G > 255] = 255
-    # G = G.astype(np.uint8)
-     
-    # B[B < 0] = 0
-    # B[B > 255] = 255
-    # B = B.astype(np.uint8)
-
-    # result = np.zeros(img.shape,dtype=img.dtype)
-    # tmp = []
-    # tmp.append(R)
-    # tmp.append(G)
-    # tmp.append(B)
-    # result = cv2.merge(tmp,result)
     cv2.imwrite(f'{title}_result.jpg', result)
     return result
 
 def main():
-    img_path = 'budda_original.png'
+    img_path = 'output/budda/budda_original.png'
     img = read_image(img_path)
-    underwater_image_enhancement(img, re.split('[_.]', img_path)[0])#img_path.split(['_', '.'])[0])
+    compensate_blue = True
+    gamma_factor = 1.5
+    kernel_size = 7
+    laplacian_levels = 3
+    underwater_image_enhancement(img, re.split('[_.]', img_path)[0], compensate_blue, gamma_factor, kernel_size, laplacian_levels)#img_path.split(['_', '.'])[0])
     print("Done")
 if __name__ == '__main__':
     main()
